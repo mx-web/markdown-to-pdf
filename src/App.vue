c499d2a63ea01d63ad867426ae36429a8126c9a4
Max Walter <walt@walt-dev.de>
2019-09-16 18:28:26 +0200

    Added Packages

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,9 @@
 <script>
 
 export default {
-  name: 'app'
+  name: 'app',
+  mounted: function() {
+    
+  }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -3,6 +3,16 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
+var pdfMake = require("pdfmake/build/pdfmake");
+var pdfFonts = require("pdfmake/build/vfs_fonts");
+pdfMake.vfs = pdfFonts.pdfMake.vfs;
+
+var htmlToPdfMake = require("html-to-pdfmake");
+
+var 
+
+
+
 new Vue({
   render: h => h(App),
 }).$mount('#app')
